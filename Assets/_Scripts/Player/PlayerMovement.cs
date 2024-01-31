1d2a416fab4d02eb825d8550baeecd0c00493520
TomasYrgo <150817213+TomasYrgo@users.noreply.github.com>
2024-01-31 14:40:20 +0100

    Update PlayerMovement.cs

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private RigidBody rb;
-
+    private Rigidbody2D rb;
+    
+    float impulseForce = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        rb = getComponent<RigidBody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void Jump()
     {
-        Vector3 impile = new Vector3(impulseForce, of, of);
-        object value = rb.AddForce(impulse, ForceMode, Impulse);
+        Vector2 impulse = new Vector3(0f, impulseForce);
+        rb.AddForce(impulse, ForceMode2D.Impulse);
     }
 }
