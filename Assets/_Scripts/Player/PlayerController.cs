146a142036dcf55ba003d80ce0959c01331f1fb1
TomasYrgo <150817213+TomasYrgo@users.noreply.github.com>
2024-02-06 09:53:35 +0100

    Stabilizing bird jump

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
+
+/// <summary>
+/// Controls the player inputs
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
-    PlayerMovement pm;
+    private PlayerMovement pm;
 
     // Start is called before the first frame update
     void Start()
@@ -12,7 +16,9 @@ public class PlayerController : MonoBehaviour
         pm = GetComponent<PlayerMovement>();
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Called once per frame. Check for the Space key press to call for the Jump method in playermovment.
+    /// </summary>
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -20,6 +26,4 @@ public class PlayerController : MonoBehaviour
             pm.Jump();
         }
     }
-
-
 }
--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls the movement of the player character.
+/// </summary>
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    
-    float impulseForce = 10f;
-    // Start is called before the first frame update
+    [SerializeField] private float impulseForce;
+
+    /// <summary>
+    /// Called before the first frame update. Initializes the Rigidbody2D component.
+    /// </summary>
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    /// <summary>
+    /// Handles character jumping by applying an upward force, the impulseforce is set in unity editor.
+    /// </summary>
     public void Jump()
     {
-        Vector2 impulse = new Vector3(0f, impulseForce);
-        rb.AddForce(impulse, ForceMode2D.Impulse);
+        rb.velocity = Vector2.up * impulseForce; 
     }
 }
