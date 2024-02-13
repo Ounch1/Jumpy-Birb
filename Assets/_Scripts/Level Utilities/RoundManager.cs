18149de52f9e1a9aeb2d6ed571cd443dfaabcce3
Ilja Grigorjev <tomkapisos@gmail.com>
2024-02-13 10:44:49 +0100

    Update RoundManager.cs

--- Assets/_Scripts/Level Utilities/RoundManager.cs
+++ Assets/_Scripts/Level Utilities/RoundManager.cs
@@ -68,4 +68,6 @@ public class RoundManager : MonoBehaviour
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
+
+    // hello
 }
